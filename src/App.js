aaea60bfd13f59ace06b672d966962ec2d4a20e3
tuongdang0406 <tuongdang0406@gmail.com>
2020-05-19 10:33:44 +0700

    create sign in and register form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Clarifai from "clarifai";
 import Navigation from "./components/Navigation/Navigation";
+import SignIn from "./components/SignIn/SignIn";
+import Register from "./components/Register/Register";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Amount from "./components/Amount/Amount";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Particles from "react-particles-js";
+
+const app = new Clarifai.App({
+  apiKey: "ca9cdc7e743e4762b732c736861f1877",
+});
 
 const particlesOptions = {
   particles: {
@@ -19,16 +27,76 @@ const particlesOptions = {
   },
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particles" params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Amount />
-      <ImageLinkForm />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imgUrl: "",
+      box: {},
+      route: "signIn",
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    // console.log(clarifaiFace);
+    const image = document.getElementById("InputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+    };
+  };
+
+  displayBox = (box) => {
+    this.setState({ box: box });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imgUrl: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) => this.displayBox(this.calculateFaceLocation(response)))
+      .catch((err) => console.log(err));
+  };
+
+  onRouteChange = (route) => {
+    this.setState({ route: route });
+  };
+
+  render() {
+    const { imgUrl, box, route } = this.state;
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        {route === "home" ? (
+          <div>
+            <Navigation onRouteChange={this.onRouteChange} />
+            <Logo />
+            <Amount />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition box={box} imgUrl={imgUrl} />
+          </div>
+        ) : route === "signIn" ? (
+          <SignIn onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
-    <div>
-      <p className="f3">{"Boooooooooooooooooooooooooooooooo"}</p>
-      <div className="center">
-        <div className="form center pa4 br3 shadow-5">
-          <input
-            className="f4 pa2 w-70 center"
-            type="text"
-            placeholder="Link..."
-          ></input>
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-green">
-            Detect
-          </button>
-        </div>
+    <div className="center">
+      <div className="form center pa4 br3 shadow-5">
+        <input
+          className="f4 pa2 w-70 center"
+          type="text"
+          placeholder="Type image URL here..."
+          onChange={onInputChange}
+        ></input>
+        <button
+          className="w-30 grow f4 link ph3 pv2 dib white bg-green"
+          onClick={onButtonSubmit}
+        >
+          Detect
+        </button>
       </div>
     </div>
   );
